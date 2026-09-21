6573518ef7818ca5f0e3e0a50eb3da86d8403c7e
joaocarlospaz <joaocarlosdapaz2006@gmail.com>
2026-09-20 22:21:43 -0300

    Algumas mudanças
    Criei uma pasta pras coisas do git, exclui umas coisas e tô testando conceitos do livro.

---
+++ poo/exercicio_carro.py
@@ -0,0 +1,31 @@
+# Exercício com classes
+# 1 - Crie uma classe Carro (Nome)
+# 2 - Crie uma classe Motor (Nome)
+# 3 - Crie uma classe Fabricante (Nome)
+# 4 - Faça a ligação entre Carro tem um Motor
+# Obs.: Um motor pode ser de vários carros
+# 5 - Faça a ligação entre Carro e um Fabricante
+# Obs.: Um fabricante pode fabricar vários carros
+# Exiba o nome do carro, motor e fabricante na tela
+
+class Carro:
+    def __init__(self, nome):
+        self.nome = nome
+
+class Motor:
+    def __init__(self, ligar = True):
+        self.ligar = ligar
+
+    def ligou():
+        ...
+
+
+
+class Fabricante:
+    def __init__(self):
+        pass
+
+
+carro = Carro("Ford")
+
+
